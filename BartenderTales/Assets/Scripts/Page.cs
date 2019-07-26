e6a248b22d7a486dd26f143bf019fc33624aa4a7
JakobiTalbot <jakobi.talbot@gmail.com>
2019-07-26 14:15:18 +1000

    - Working book

---
+++ BartenderTales/Assets/Scripts/Page.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Page : MonoBehaviour
+{
+    private Collider m_collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, Vector3.up);
+        if (Physics.Raycast(ray, out var hit, 1f)
+            && hit.transform.GetComponent<Page>())
+        {
+            m_collider.enabled = false;
+        }
+        else
+        {
+            m_collider.enabled = true;
+        }
+    }
+}
