6403b9c1501f5c807ba4ad0eac9de882d6091359
Jakobi Talbot <jakobi.talbot@gmail.com>
2019-08-09 15:04:32 +1000

    - Added random idle animations

---
+++ BartenderTales/Assets/Scripts/CustomerAnimators/CustomerAnimator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class CustomerAnimator : MonoBehaviour
+{
+    protected Animator m_animator;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    abstract public IEnumerator IdleLoop();
+}
---
+++ BartenderTales/Assets/Scripts/CustomerAnimators/GoblinAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinAnimator : CustomerAnimator
+{
+    [SerializeField]
+    private Vector2 m_minMaxSecondsForRandomIdleAnimation = new Vector2(4f, 8f);
+    [SerializeField]
+    private string[] m_randomIdleAnimationTriggerNames;
+
+    override public IEnumerator IdleLoop()
+    {
+        // while idle
+        while (m_animator.GetBool("StoppedMoving"))
+        {
+            // wait for random time
+            yield return new WaitForSeconds(Random.Range(m_minMaxSecondsForRandomIdleAnimation.x, m_minMaxSecondsForRandomIdleAnimation.y));
+            // then play random idle animation
+            m_animator.SetTrigger(m_randomIdleAnimationTriggerNames[Random.Range(0, m_randomIdleAnimationTriggerNames.Length)]);
+        }
+    }
+}
