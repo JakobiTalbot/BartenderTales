443a2619c15721b3a3def10b2d6b82956ab3a33d
JakobiTalbot <jakobi.talbot@gmail.com>
2019-08-14 14:27:13 +1000

    - Updated button script

--- BartenderTales/Assets/Scripts/Button.cs
+++ BartenderTales/Assets/Scripts/Button.cs
@@ -2,16 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Valve.VR.InteractionSystem;
 
 public class Button : MonoBehaviour
 {
     [SerializeField]
     private UnityEvent m_onButtonPressed;
+
+    private Vector3 m_lastHandPos;
+    private GameObject m_activeHand;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<ButtonBottom>())
+        if (other.GetComponent<Hand>())
+        {
+            StartCoroutine(UpdateButtonPress());
+            m_activeHand = other.gameObject;
+            m_lastHandPos = m_activeHand.transform.position;
+        }
+        else if (other.GetComponent<ButtonBottom>())
         {
             m_onButtonPressed.Invoke();
+            StopCoroutine(UpdateButtonPress());
+        }
+    }
+
+    private IEnumerator UpdateButtonPress()
+    {
+        while (true)
+        {
+            transform.position += -transform.up * (m_lastHandPos.y - m_activeHand.transform.position.y);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Hand>())
+        {
+            StopCoroutine(UpdateButtonPress());
+            m_activeHand = null;
         }
     }
 }
