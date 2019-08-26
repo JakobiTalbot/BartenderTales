7ef11b4f8dd34df4d834d2ce36ff781b37d0f1a8
Jakobi Talbot <jakobi.talbot@gmail.com>
2019-08-26 22:13:29 +1000

    - Happy hour mechanic

--- BartenderTales/Assets/Scripts/CustomerSpawner.cs
+++ BartenderTales/Assets/Scripts/CustomerSpawner.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class CustomerSpawner : MonoBehaviour
 {
+    // possible refactor: create array of points and sort by distance to bar, spawn customer at lowest distance
+
     public GameObject[] m_customerPrefabs;
     public Transform m_spawnPoint;
     public List<Transform> m_servingPoints;
     public List<Transform> m_waitingPoints;
-    public float m_timeBetweenCustomers = 60f;
     public Transform m_coinDropPoint;
+
+    [SerializeField]
+    private Vector2 m_randomRangeBetweenHappyHours;
+    [SerializeField]
+    private Vector2 m_randomLengthForHappyHour;
+    [SerializeField]
+    [Tooltip("The random range of time between a customer spawning during happy hour")]
+    private Vector2 m_randomRangeCustomerSpawnHappyHour;
+    [SerializeField]
+    [Tooltip("The random range of time between a customer spawning not during happy hour")]
+    private Vector2 m_randomRangeCustomerSpawnNotHappyHour;
 
     [HideInInspector]
     public List<GameObject> m_customers;
     [HideInInspector]
     public bool m_spawnCustomers = true;
     private float m_fCustomerSpawnTimer = 0f;
-
+    private bool m_bHappyHour = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,42 +41,74 @@ public class CustomerSpawner : MonoBehaviour
         cust.SetDestination(m_servingPoints[i], false);
         cust.SetCoinDropPos(m_coinDropPoint.position);
         m_servingPoints.RemoveAt(i);
-
+        StartCoroutine(CustomerSpawnLoop());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CustomerSpawnLoop()
     {
-        if (!m_spawnCustomers)
-            return;
-        // increment timer
-        m_fCustomerSpawnTimer += Time.deltaTime;
-
-        // if time to spawn customer and spawn points available
-        while (m_fCustomerSpawnTimer >= m_timeBetweenCustomers
-            && (m_servingPoints.Count > 0 || m_waitingPoints.Count > 0))
+        while (true)
         {
-            Transform destPoint;
-            bool bWait = false;
-            // if no free serving points
-            if (m_servingPoints.Count <= 0)
-            {
-                destPoint = m_waitingPoints[m_waitingPoints.Count - 1];
-                m_waitingPoints.RemoveAt(m_waitingPoints.Count - 1);
-                bWait = true;
-            }
-            else // if there are free serving points
-            {
-                destPoint = m_servingPoints[m_servingPoints.Count - 1];
-                m_servingPoints.RemoveAt(m_servingPoints.Count - 1);
-            }
+            if (m_bHappyHour)
+                yield return new WaitForSeconds(Random.Range(m_randomRangeCustomerSpawnHappyHour.x, m_randomRangeCustomerSpawnHappyHour.y));
+            else
+                yield return new WaitForSeconds(Random.Range(m_randomRangeCustomerSpawnNotHappyHour.x, m_randomRangeCustomerSpawnNotHappyHour.y));
 
-            m_fCustomerSpawnTimer -= m_timeBetweenCustomers;
-            // spawn customer
-            m_customers.Add(Instantiate(m_customerPrefabs[Random.Range(0, m_customerPrefabs.Length)], m_spawnPoint.position, m_spawnPoint.rotation));
-            Customer cust = m_customers[m_customers.Count - 1].GetComponent<Customer>();
-            cust.SetDestination(destPoint, bWait);
-            cust.SetCoinDropPos(m_coinDropPoint.position);
+            // spawn customer when available
+            while (!SpawnCustomer())
+                yield return new WaitForEndOfFrame();
         }
+    }
+
+    private IEnumerator HappyHourTimer()
+    {
+        while (true)
+        {
+            if (!m_bHappyHour)
+            {
+                yield return new WaitForSeconds(Random.Range(m_randomRangeBetweenHappyHours.x, m_randomRangeBetweenHappyHours.y));
+                // happy hour
+                m_bHappyHour = true;
+                // restart customer spawn coroutine
+                StopCoroutine(CustomerSpawnLoop());
+                StartCoroutine(CustomerSpawnLoop());
+            }
+            else
+            {
+                yield return new WaitForSeconds(Random.Range(m_randomLengthForHappyHour.x, m_randomLengthForHappyHour.y));
+                m_bHappyHour = false;
+            }
+        }
+    }
+
+    private bool SpawnCustomer()
+    {
+        // dont spawn if there is no spaces
+        if (!(m_servingPoints.Count > 0 || m_waitingPoints.Count > 0))
+            return false;
+
+        // create variables
+        Transform destPoint;
+        bool bWait = false;
+
+        // if no free serving points
+        if (m_servingPoints.Count <= 0)
+        {
+            destPoint = m_waitingPoints[m_waitingPoints.Count - 1];
+            m_waitingPoints.RemoveAt(m_waitingPoints.Count - 1);
+            bWait = true;
+        }
+        else // if there are free serving points
+        {
+            destPoint = m_servingPoints[m_servingPoints.Count - 1];
+            m_servingPoints.RemoveAt(m_servingPoints.Count - 1);
+        }
+
+        // spawn customer
+        m_customers.Add(Instantiate(m_customerPrefabs[Random.Range(0, m_customerPrefabs.Length)], m_spawnPoint.position, m_spawnPoint.rotation));
+        Customer cust = m_customers[m_customers.Count - 1].GetComponent<Customer>();
+        cust.SetDestination(destPoint, bWait);
+        cust.SetCoinDropPos(m_coinDropPoint.position);
+
+        return true;
     }
 }
