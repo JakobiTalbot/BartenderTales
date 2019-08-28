f9bbf1f686cb4e44bcd974cf8f55922b55d2c2bd
JakobiTalbot <jakobi.talbot@gmail.com>
2019-08-28 13:14:16 +1000

    - Fixed bug in MoneyJar.cs

--- BartenderTales/Assets/Scripts/MoneyJar.cs
+++ BartenderTales/Assets/Scripts/MoneyJar.cs
@@ -20,14 +20,18 @@ public class MoneyJar : MonoBehaviour
     [HideInInspector]
     public int m_nCurrentMoney = 0;
 
+    private bool m_bAlreadyWon = false;
+
     public void AddMoney(int nMoney)
     {
         m_nCurrentMoney += nMoney;
         // set text on money counter
         m_moneyCounter.text = m_nCurrentMoney.ToString();
-        if (m_nCurrentMoney >= m_maxMoney)
+        if (m_nCurrentMoney >= m_maxMoney
+            && !m_bAlreadyWon)
         {
             StartCoroutine(WinGame());
+            m_bAlreadyWon = true;
         }
     }
 
