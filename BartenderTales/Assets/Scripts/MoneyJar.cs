01211ecf3929803befa40037295d28b4fd9d57fe
JakobiTalbot <jakobi.talbot@gmail.com>
2019-08-28 11:39:08 +1000

    - New tip jar prefab
    - Tip jar now displays current money

--- BartenderTales/Assets/Scripts/MoneyJar.cs
+++ BartenderTales/Assets/Scripts/MoneyJar.cs
@@ -1,32 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoneyJar : MonoBehaviour
 {
     [SerializeField]
     private int m_maxMoney = 10;
+    [SerializeField]
+    private TextMeshProUGUI m_moneyCounter;
 
-    private Renderer m_renderer;
     [HideInInspector]
     public int m_nCurrentMoney = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_renderer = GetComponent<Renderer>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void AddMoney(int nMoney)
     {
         m_nCurrentMoney += nMoney;
-        m_renderer.material.SetFloat("_MoneyAmount", (float)m_nCurrentMoney / m_maxMoney);
+        // set text on money counter
+        m_moneyCounter.text = m_nCurrentMoney.ToString();
         if (m_nCurrentMoney >= m_maxMoney)
         {
             // win game
