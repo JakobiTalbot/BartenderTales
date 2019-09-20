f4f30df49c94304090802d99c060abdfdd5d0d2c
JakobiTalbot <jakobi.talbot@gmail.com>
2019-09-20 15:13:28 +1000

    - Horn selector

---
+++ BartenderTales/Assets/Scripts/HornSelector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HornSelector : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] m_horns;
+
+    private void Awake()
+    {
+        // enable random horn on creation
+        int i = Random.Range(0, m_horns.Length);
+        m_horns[i].SetActive(true);
+    }
+}
