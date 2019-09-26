a309219b46e7cb80789d442d8dce482a74e23fe7
Tory-Kruger <krugertory@gmail.com>
2019-09-26 16:52:01 +1000

    Trying to make the hands anim blend tree. Adams squezzeometre

--- BartenderTales/Assets/Mesh/Hands/HandsAnim.cs
+++ BartenderTales/Assets/Mesh/Hands/HandsAnim.cs
@@ -8,23 +8,34 @@ public class HandsAnim : MonoBehaviour
    // public SteamVR_Input_Sources hand;
 
     public Animator anim;
+    public SteamVR_TrackedObject mTrackedObject = null;
+    public SteamVR mDevice;
+
+
+
+    private void Awake()    
+    {
+        mTrackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         //SteamVR_Actions._default.GrabGrip.GetLastStateDown(SteamVR_Input_Sources.LeftHand);
 
+        Debug.Log("Left Trigger value:" + SteamVR_Actions.default_Squeeze.GetAxis(SteamVR_Input_Sources.LeftHand));// SteamVR_Input_Sources.LeftHand));
         if (SteamVR_Actions._default.GrabPinch.GetStateDown(SteamVR_Input_Sources.LeftHand))
         {
-            Debug.Log("Predded left6");
-            GetComponent<Animator>().SetTrigger("Grab");
+            Debug.Log("Predded left");
+            Debug.Log("Left Trigger value:" + SteamVR_Actions._default.Squeeze.GetAxis(SteamVR_Input_Sources.LeftHand).ToString());
+            GetComponent<Animator>().SetTrigger("Grab1");
         }
 
-        //SteamVR_Actions._default.GrabGrip.GetLastStateDown(SteamVR_Input_Sources.RightHand);
-        if (SteamVR_Actions._default.GrabPinch.GetLastStateDown(SteamVR_Input_Sources.RightHand))
+        ////SteamVR_Actions._default.GrabGrip.GetLastStateDown(SteamVR_Input_Sources.RightHand);
+        if (SteamVR_Actions._default.GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
         {
             Debug.Log("Predded right");
-            GetComponent<Animator>().SetTrigger("Grab");
+            GetComponent<Animator>().SetTrigger("Grab2");
         }
     }
 }
---
+++ BartenderTales/Assets/Scripts/AdamsSqueezometre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class AdamsSqueezometre : MonoBehaviour
+{
+    public SteamVR_Input_Sources LeftInputSource = SteamVR_Input_Sources.LeftHand;
+    public SteamVR_Input_Sources RightInputSource = SteamVR_Input_Sources.RightHand;
+
+    private void Update()
+    {
+        Debug.Log("Left Trigger value:" + LeftInputSource.ToString() + SteamVR_Actions._default.Squeeze.GetAxis(LeftInputSource).ToString());
+        Debug.Log("Right Trigger value:" + RightInputSource.ToString() + SteamVR_Actions._default.Squeeze.GetAxis(RightInputSource).ToString());
+    }
+
+}
+
