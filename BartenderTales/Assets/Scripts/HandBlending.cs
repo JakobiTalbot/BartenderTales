106542af155dcdf1bdc012acec1885c2e061193c
JakobiTalbot <jakobi.talbot@gmail.com>
2019-10-10 14:59:45 +1100

    - Hand blending anims

---
+++ BartenderTales/Assets/Scripts/HandBlending.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HandBlending : MonoBehaviour
+{
+    [SerializeField]
+    private SteamVR_Input_Sources m_inputSource;
+
+    private Animator m_animator;
+
+    private void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        m_animator.SetFloat("BlendHandFloat", SteamVR_Actions._default.Squeeze.GetAxis(m_inputSource));
+    }
+}
