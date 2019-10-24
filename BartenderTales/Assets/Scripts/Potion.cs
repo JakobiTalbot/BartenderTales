265dd9231d1356977018a22731533c4e06d98aec
JakobiTalbot <jakobi.talbot@gmail.com>
2019-10-25 09:26:40 +1100

    - Glass clinking sound
    - Changed collision detection on potion prefabs
    - Removed old potion prefabs

--- BartenderTales/Assets/Scripts/Potion.cs
+++ BartenderTales/Assets/Scripts/Potion.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 public class Potion : MonoBehaviour
 {
     public PotionName m_potionName;
+    [SerializeField]
+    private float m_impulseThresholdToPlayAudio = 0.5f;
+    [SerializeField]
+    private AudioClip[] m_collisionAudioClips;
 
     private Transform m_point;
+    private AudioSource m_audioSource;
     private bool m_bDrank = false;
+
+    private void Awake()
+    {
+        m_audioSource = GetComponent<AudioSource>();
+    }
 
     public void SetPoint(Transform point)
     {
@@ -22,6 +32,15 @@ public class Potion : MonoBehaviour
         {
             m_bDrank = true;
             cust.DrinkPotion(this);
+        }
+        else
+        {
+            Debug.Log(collision.impulse.magnitude);
+            if (collision.impulse.magnitude > m_impulseThresholdToPlayAudio)
+            {
+                m_audioSource.volume = collision.impulse.magnitude / 5f;
+                m_audioSource.PlayOneShot(m_collisionAudioClips[Random.Range(0, m_collisionAudioClips.Length)]);
+            }
         }
     }
 }
