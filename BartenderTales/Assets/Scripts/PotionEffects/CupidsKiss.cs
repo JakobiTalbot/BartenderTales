6ee217796033baca8ccf295f7171d81e7229d9fc
JakobiTalbot <jakobi.talbot@gmail.com>
2019-10-31 09:17:08 +1100

    - Fixed cupids kiss particles

--- BartenderTales/Assets/Scripts/PotionEffects/CupidsKiss.cs
+++ BartenderTales/Assets/Scripts/PotionEffects/CupidsKiss.cs
@@ -19,14 +19,13 @@ public class CupidsKiss : PotionEffect
         PotionAssets pa = FindObjectOfType<PotionAssets>();
         // enable ragdoll
         m_cust.SetRagdoll(true);
-        // particles
-        GameObject p = Instantiate(pa.m_cupidsKissParticlePrefab, transform);
-        // play audio
-        GetComponent<AudioSource>().PlayOneShot(pa.m_cupidsKissActivationAudio);
-        p.transform.position = transform.position;
-        //p.transform.rotation = transform.rotation;
-        Destroy(p, 5f);
-
+        // start dissolve before enabling particles to prevent particle colour changing
         StartCoroutine(m_cust.Dissolve());
+        // instantiate particles childed to central rigidbody of customer
+        GameObject p = Instantiate(pa.m_cupidsKissParticlePrefab, m_cust.m_pickMeUpRigidbody.transform);
+        p.transform.SetPositionAndRotation(m_cust.m_pickMeUpRigidbody.transform.position, Quaternion.LookRotation(-m_cust.m_pickMeUpRigidbody.transform.right));
+        // play audio clip
+        GetComponent<AudioSource>().PlayOneShot(pa.m_cupidsKissActivationAudio);
+        Destroy(p, 5f);
     }
 }
