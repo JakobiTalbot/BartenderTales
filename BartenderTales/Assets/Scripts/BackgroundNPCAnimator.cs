ee156cb974aa74bc6abe6513803cfc04ba48e860
JakobiTalbot <jakobi.talbot@gmail.com>
2019-11-13 10:34:21 +1100

    - Background npc animator script

---
+++ BartenderTales/Assets/Scripts/BackgroundNPCAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundNPCAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_randomTriggerNames;
+    [SerializeField]
+    private Vector2 m_randomRangeBetweenTriggering;
+
+    private Animator m_animator;
+
+    void Start()
+    {
+        // get reference to animator
+        m_animator = GetComponent<Animator>();
+        // start loop
+        StartCoroutine(IdleLoop());
+    }
+
+    private IEnumerator IdleLoop()
+    {
+        while (true)
+        {
+            // wait for random seconds before triggering
+            yield return new WaitForSeconds(Random.Range(m_randomRangeBetweenTriggering.x, m_randomRangeBetweenTriggering.y));
+
+            // trigger random animation
+            m_animator.SetTrigger(m_randomTriggerNames[Random.Range(0, m_randomTriggerNames.Length)]);
+        }
+    }
+}
